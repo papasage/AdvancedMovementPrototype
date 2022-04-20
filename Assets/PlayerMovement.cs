097c321af898a5551808cdbcb00a687dc53fcb1b
Ian Guglielmi-White <ianrgwhite@gmail.com>
2022-04-20 16:05:05 -0500

    Sprint/Crouch Added + Slope Movement WIP

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,10 +8,10 @@ public class PlayerMovement : MonoBehaviour
     [Header("Keybinds")]
     public KeyCode jumpKey = KeyCode.Space;
     public KeyCode sprintKey = KeyCode.LeftShift;
-
+    public KeyCode crouchKey = KeyCode.LeftControl;
     [Header("Movement")]
     private float moveSpeed;
-    public enum MovementState { walking, sprinting, air, }
+    public enum MovementState { walking, sprinting, crouching, air, }
     public MovementState state;
     public float walkSpeed;
     public float sprintSpeed;
@@ -22,14 +22,19 @@ public class PlayerMovement : MonoBehaviour
     public float jumpCooldown;
     public float airMultiplier;
     public bool readyToJump;
-
-    
-
-
+    [Header("Crouching")]
+    public float crouchSpeed;
+    public float crouchYScale;
+    private float startYScale;
+    //--------------------------------------------------
     [Header("GroundCheck")]
     public float playerHeight;
     public LayerMask whatIsGround;
     bool grounded;
+
+    [Header("Slope Handling")]
+    public float maxSlopeAngle;
+    private RaycastHit slopeHit;
 
     [Header("Orientation Point on Player")]
     public Transform orientation;
@@ -40,7 +45,7 @@ public class PlayerMovement : MonoBehaviour
     Vector3 moveDirection;
 
     Rigidbody rb;
-
+    //-------------------------------------------------
     [Header("Speedometer")]
     public Text SpeedText;
 
@@ -50,6 +55,9 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         //we freeze the rotations so that it doesn't fall over. we are controlling every move the player makes
         rb.freezeRotation = true;
+
+        //save the starting player height for returning from a crouch
+        startYScale = transform.localScale.y;
     }
 
     private void Update()
@@ -95,10 +103,32 @@ public class PlayerMovement : MonoBehaviour
             //Jump will repeat if key is held, but at a jumpCooldown interval.
             //Invoke(nameof(ResetJump), jumpCooldown);
         }
+
+        //CROUCH START
+        if (Input.GetKeyDown(crouchKey))
+        {
+            //when the player crouches, they shrink on the y
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+            //the player shrinks to center, so apply force to get low quickly
+            rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+        }
+        //CROUCH STOP
+        if (Input.GetKeyUp(crouchKey))
+        {
+            //return the player to normal size when key is released
+            //"normal size" is collected on Start()
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+        }
     }
 
     private void StateHandler()
     {
+        //Mode-Crouching
+        if (Input.GetKey(crouchKey))
+        {
+            state = MovementState.crouching;
+            moveSpeed = crouchSpeed;
+        }
         //Mode-Sprinting
         if(grounded && Input.GetKey(sprintKey))
         {
@@ -124,6 +154,17 @@ public class PlayerMovement : MonoBehaviour
         //movement direction is FORWARD/BACKWARD movement + LEFT/RIGHT movement
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
+        //on slope
+        if(OnSlope())
+        {
+            rb.AddForce(GetSlopeMoveDirection() * moveSpeed * 10f, ForceMode.Force);
+
+            if (rb.velocity.y > 0)
+            {
+                rb.AddForce(Vector3.down * 80f);
+            }
+        }
+
         //move our rigidbody in the move direction * moveSpeed
         // vector3.normalized will return the same direction, but with a length of 1.0
 
@@ -134,6 +175,9 @@ public class PlayerMovement : MonoBehaviour
         //in the air apply a modified force with airMultiplier
         else if (!grounded)
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+
+        //turn gravity off while on a slope
+        rb.useGravity = !OnSlope();
     }
 
     private void Jump()
@@ -149,6 +193,21 @@ public class PlayerMovement : MonoBehaviour
     private void ResetJump()
     {
         readyToJump = true;
+    }
+
+    private bool OnSlope()
+    {
+        if(Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight *0.5f + 0.3f))
+        {
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            return angle < maxSlopeAngle && angle != 0;
+        }
+        return false;
+    }
+
+    private Vector3 GetSlopeMoveDirection()
+    {
+        return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
     }
 
     private void SpeedControl()
