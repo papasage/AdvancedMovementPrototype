6ed050c8becf781f10587b7e176390eb7a2c2b51
Ian Guglielmi-White <ianrgwhite@gmail.com>
2022-04-21 14:13:26 -0500

    Working on Slopes and Slides

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,18 +5,25 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // ----------------------------------------------------------------------------------------------------------------------------
+    // VARIABLES
+    // ----------------------------------------------------------------------------------------------------------------------------
     [Header("Keybinds")]
     public KeyCode jumpKey = KeyCode.Space;
     public KeyCode sprintKey = KeyCode.LeftShift;
     public KeyCode crouchKey = KeyCode.LeftControl;
+    public KeyCode slideKey = KeyCode.LeftControl;
     [Header("Movement")]
     private float moveSpeed;
-    public enum MovementState { walking, sprinting, crouching, air, }
+    public enum MovementState { walking, sprinting, crouching, sliding, air, }
     public MovementState state;
     public float walkSpeed;
     public float sprintSpeed;
+    public float slideSpeed;
     public float wallrunSpeed;
     public float groundDrag;
+    private float desiredMoveSpeed;
+    private float lastDesiredMoveSpeed;
     [Header("Jumping")]
     public float jumpForce;
     public float jumpCooldown;
@@ -26,28 +33,32 @@ public class PlayerMovement : MonoBehaviour
     public float crouchSpeed;
     public float crouchYScale;
     private float startYScale;
-    //--------------------------------------------------
-    [Header("GroundCheck")]
-    public float playerHeight;
-    public LayerMask whatIsGround;
-    bool grounded;
-
+    [Header("Sliding")]
+    public float maxSlideTime;
+    public float slideForce;
+    public float slideDrag;
+    private float slideTimer;
+    private bool sliding;
+    private Vector3 slideDirection;
     [Header("Slope Handling")]
     public float maxSlopeAngle;
     private RaycastHit slopeHit;
-
-    [Header("Orientation Point on Player")]
-    public Transform orientation;
-
-    float horizontalInput;
-    float verticalInput;
-
-    Vector3 moveDirection;
-
-    Rigidbody rb;
-    //-------------------------------------------------
+    bool exitingSlope;
     [Header("Speedometer")]
     public Text SpeedText;
+    [Header("Additional Variables")]
+    public float playerHeight;
+    public Transform orientation;
+    public LayerMask whatIsGround;
+    bool grounded;
+    float horizontalInput;
+    float verticalInput;
+    Vector3 moveDirection;
+    Rigidbody rb;
+
+    // ----------------------------------------------------------------------------------------------------------------------------
+    // START / UPDATE / FIXED UPDATE
+    // ----------------------------------------------------------------------------------------------------------------------------
 
     void Start()
     {
@@ -70,12 +81,17 @@ public class PlayerMovement : MonoBehaviour
         StateHandler();
 
         //handle drag & reset jump when grounded
-        if (grounded)
+        if (grounded && !sliding)
         {
             rb.drag = groundDrag;
             ResetJump();
         }
-            
+        else if (grounded && sliding)
+        {
+            rb.drag = slideDrag;
+            ResetJump();
+        }
+         
         else
             rb.drag = 0;
 
@@ -85,14 +101,24 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         MovePlayer();
+
+        if (sliding)
+        {
+            verticalInput = 0;
+            horizontalInput = 0;
+            SlidingMovement();
+        }
+            
     }
 
+    // ----------------------------------------------------------------------------------------------------------------------------
+    // OTHER METHODS & THINGS
+    // ----------------------------------------------------------------------------------------------------------------------------
     private void MyInput()
     {
-        //this function is collecting keyboard input data
+        //WALK
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
-
         //JUMP
         if(Input.GetKeyDown(jumpKey) && readyToJump)
         {
@@ -103,7 +129,6 @@ public class PlayerMovement : MonoBehaviour
             //Jump will repeat if key is held, but at a jumpCooldown interval.
             //Invoke(nameof(ResetJump), jumpCooldown);
         }
-
         //CROUCH START
         if (Input.GetKeyDown(crouchKey))
         {
@@ -119,28 +144,49 @@ public class PlayerMovement : MonoBehaviour
             //"normal size" is collected on Start()
             transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
         }
+        //SLIDE START
+        if (Input.GetKeyDown(slideKey) && (horizontalInput != 0 || verticalInput != 0))
+        {
+            Vector3 slideDirection = moveDirection;
+            StartSlide();
+        }
+        //SLIDE STOP
+        if (Input.GetKeyUp(slideKey) && sliding)
+        {
+            StopSlide();
+        }
     }
-
     private void StateHandler()
     {
+        //Mode-Sliding
+        if (sliding)
+        {
+            state = MovementState.sliding;
+            if (OnSlope() && rb.velocity.y < 0.1f)
+            {
+                desiredMoveSpeed = slideSpeed;
+            }
+            else desiredMoveSpeed = sprintSpeed;
+
+        }
         //Mode-Crouching
-        if (Input.GetKey(crouchKey))
+        else if (Input.GetKey(crouchKey))
         {
             state = MovementState.crouching;
-            moveSpeed = crouchSpeed;
+            desiredMoveSpeed = crouchSpeed;
         }
         //Mode-Sprinting
-        if(grounded && Input.GetKey(sprintKey))
+        else if(grounded && Input.GetKey(sprintKey))
         {
             state = MovementState.sprinting;
-            moveSpeed = sprintSpeed;
+            desiredMoveSpeed = sprintSpeed;
         }
 
         //Mode-Walking
         else if (grounded)
         {
             state = MovementState.walking;
-            moveSpeed = walkSpeed;
+            desiredMoveSpeed = walkSpeed;
         }
 
         //Mode-Air
@@ -148,6 +194,35 @@ public class PlayerMovement : MonoBehaviour
         {
             state = MovementState.air;
         }
+
+        //check if the desiredMoveSpeed has changed drastically
+        if(Mathf.Abs(desiredMoveSpeed - lastDesiredMoveSpeed) > 4f && moveSpeed !=0)
+        { 
+            StopAllCoroutines();
+            StartCoroutine(SmoothlyLerpMoveSpeed());
+        }
+        else
+        {
+            moveSpeed = desiredMoveSpeed;
+        }
+
+        lastDesiredMoveSpeed = desiredMoveSpeed;
+    }
+    private IEnumerator SmoothlyLerpMoveSpeed()
+    {
+        //smoothly lerp movementSpeed to desired value
+        float time = 0;
+        float difference = Mathf.Abs(desiredMoveSpeed - moveSpeed);
+        float startValue = moveSpeed;
+
+        while (time<difference)
+        {
+            moveSpeed = Mathf.Lerp(startValue, desiredMoveSpeed, time / difference);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        moveSpeed = desiredMoveSpeed;
     }
     private void MovePlayer()
     {
@@ -155,13 +230,13 @@ public class PlayerMovement : MonoBehaviour
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
         //on slope
-        if(OnSlope())
+        if(OnSlope() && !exitingSlope)
         {
-            rb.AddForce(GetSlopeMoveDirection() * moveSpeed * 10f, ForceMode.Force);
+            rb.AddForce(GetSlopeMoveDirection(moveDirection) * moveSpeed * 20f, ForceMode.Force);
 
             if (rb.velocity.y > 0)
             {
-                rb.AddForce(Vector3.down * 80f);
+                rb.AddForce(Vector3.down * 80f,ForceMode.Force);
             }
         }
 
@@ -179,9 +254,54 @@ public class PlayerMovement : MonoBehaviour
         //turn gravity off while on a slope
         rb.useGravity = !OnSlope();
     }
+    private bool OnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f))
+        {
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            return angle < maxSlopeAngle && angle != 0;
+        }
+        return false;
+    }
+    private Vector3 GetSlopeMoveDirection(Vector3 direction)
+    {
+        return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
+    }
+    private void SpeedControl()
+    {
+        //CONTROL SPEED ON SLOPE
+        if (OnSlope() && !exitingSlope)
+        {
+            if (rb.velocity.magnitude > moveSpeed)
+            {
+                rb.velocity = rb.velocity.normalized * moveSpeed;
+            }
+        }
 
+        //CONTROL SPEED ON GROUND OR IN AIR
+        else
+        {
+            //flatVel is only the X and Z movement speed
+            Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+            //SPEEDOMETER CODE HERE
+            float kilometerConversion = rb.velocity.magnitude * 2;
+            //display speed on HUD
+            SpeedText.text = "Speed:" + kilometerConversion.ToString("F0") + "kph";
+
+
+            //if it is greater than our moveSpeed, then recalculate what it should be and apply
+            if (flatVel.magnitude > moveSpeed)
+            {
+                Vector3 limitedVel = flatVel.normalized * moveSpeed;
+                rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+            }
+        }
+    }
     private void Jump()
     {
+        exitingSlope = true;
+
         // reset y velocity so you always jump the same height
         rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
@@ -189,43 +309,55 @@ public class PlayerMovement : MonoBehaviour
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
 
     }
-
     private void ResetJump()
     {
         readyToJump = true;
-    }
-
-    private bool OnSlope()
+        exitingSlope = false;
+    } 
+    private void StartSlide()
     {
-        if(Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight *0.5f + 0.3f))
+        sliding = true;
+        
+        //when the player crouches, they shrink on the y
+        transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+        //the player shrinks to center, so apply force to get low quickly
+        rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+
+        slideTimer = maxSlideTime;
+
+    }
+    private void SlidingMovement()
+    {
+        
+
+        //normal sliding
+        if(!OnSlope() || rb.velocity.y > -0.1f)
         {
-            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
-            return angle < maxSlopeAngle && angle != 0;
+            rb.AddForce(slideDirection.normalized * slideForce, ForceMode.Force);
+
+            //slideTimer -= Time.deltaTime;
         }
-        return false;
-    }
 
-    private Vector3 GetSlopeMoveDirection()
-    {
-        return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
-    }
-
-    private void SpeedControl()
-    {
-        //flatVel is only the X and Z movement speed
-        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-
-        //SPEEDOMETER CODE HERE
-        float kilometerConversion = flatVel.magnitude * 2;
-        //display speed on HUD
-        SpeedText.text = "Speed:" + kilometerConversion.ToString("F0") + "kph";
-
-
-        //if it is greater than our moveSpeed, then recalculate what it should be and apply
-        if (flatVel.magnitude > moveSpeed)
+        //slope sliding
+        else
         {
-            Vector3 limitedVel = flatVel.normalized * moveSpeed;
-            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+            rb.AddForce(GetSlopeMoveDirection(slideDirection) * slideForce, ForceMode.Force);
         }
+
+        if (slideTimer <= 0)
+        {
+            StopSlide();
+        }
+
     }
+    private void StopSlide()
+    {
+        sliding = false;
+        //return the player to normal size when key is released
+        //"normal size" is collected on Start()
+        transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+    }
+
+
+
 }
